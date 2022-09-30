31bf8c3a109e5f1a1f2bd57e2fe2355f1cbe3843
POLYDNK <firespert@gmail.com>
2022-09-29 20:56:32 -0700

    Grid Prototyping

---
+++ Assets/Scripts/GridBehavior.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehavior : MonoBehaviour
+{
+    [SerializeField] public uint width, height; // Width and height of the grid
+    [SerializeField] public int tilesize; // Size of each tile
+
+    public GameObject Tile; // A cell or tile that makes up the grid
+    public GameObject [,] grid; // The actual grid, represented by a 2d array
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GameObject[width, height];
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+
+    }
+
+    void GenerateGrid()
+    {
+        for (uint x = 0; x < width; x++)
+        {
+            for (uint y = 0; y < height; y++)
+            {
+                Vector3 pos = new Vector3(transform.position.x + x*tilesize, transform.position.y, transform.position.z + y*tilesize);
+                GameObject newTile = Instantiate(Tile, pos, transform.rotation);
+                grid[x, y] = newTile;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    // Materials
+    [SerializeField] Material unselected, highlighted, selected;
+
+    // Things used for click detection
+    private Camera cam;
+    private Renderer rend;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If mouse down
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Cast ray from screen point to mouse pos
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // Use physics to detect a "collision" from a ray
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                rend.sharedMaterial = selected;
+            }
+        }
+    }
+}
