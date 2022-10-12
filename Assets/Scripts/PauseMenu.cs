7382b53c767a538c11d73b54a422ada29ea38910
Gerald Achurra <GeraldAchurra@hotmail.com>
2022-10-12 16:29:42 -0700

    Added a simple PauseMenu script and functionality to be useds on overworld bartering scenes.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool PausedStatus = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+          if (PausedStatus)
+          {
+            ResumeGame();
+          }
+          else
+          {
+            PauseGame();
+          }
+
+        }
+    }
+    void ResumeGame()
+    {
+      pauseMenuUI.SetActive(false);
+      Time.timeScale = 1f;
+      PausedStatus = false;
+    }
+    void PauseGame()
+    {
+      pauseMenuUI.SetActive(true);
+      Time.timeScale = 0f;
+      PausedStatus = true;
+    }
+    public void ExitGame () //method exit the game, attach to exitgamebutton
+      {
+        Debug.Log("Exiting Adrift..."); //For testing purposes, text shows in log
+        Application.Quit(); //exits and quits the game application
+      }
+      public void LoadMenuScene()
+      {
+        SceneManager.LoadScene("TitleMenuScene");
+      }
+}
