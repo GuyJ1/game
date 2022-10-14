baacc07b2dab1d70f73733bd37348bf8ea925b81
a-smurf <gongb1@unlv.nevada.edu>
2022-10-14 02:03:29 -0700

    changed a file

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -25,10 +25,10 @@ public class Dialogue : MonoBehaviour
         if(playerInRange) {
             visualCue.SetActive(true);
 
-            // triggers the dialogue if interact button is pressed
-            // if(InputManager.GetInstance().GetInteractPressed()) {
-            //     Debug.Log(inkJSON.text);
-            // }
+            triggers the dialogue if interact button is pressed
+            if(InputManager.GetInstance().GetInteractPressed()) {
+                Debug.Log(inkJSON.text);
+            }
 
         } 
             
