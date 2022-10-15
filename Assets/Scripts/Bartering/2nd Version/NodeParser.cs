cdc7612ecbe7b564905764daef0115569ddb456d
Jacob Guy <jacobguy007@gmail.com>
2022-10-15 13:58:40 -0700

    Bartering with Xnode (added Xnode)

---
+++ Assets/Scripts/Bartering/2nd Version/NodeParser.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeParser : MonoBehaviour
+{
+    public DialogueGraph graph;
+
+    private void Start()
+    {
+        
+    }
+
+
+}
