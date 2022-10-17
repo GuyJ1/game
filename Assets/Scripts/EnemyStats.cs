bd14c8d26c2c55492115c42918b671723d475fd3
walkec12 <cxwbusiness@gmail.com>
2022-10-17 13:22:22 -0700

    Implmented Hit and Crit Rates for Enemy units (EnemyStats), and implemented a Difficulty Setting that alters some enemy stats depending on the difficulty

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -17,13 +17,13 @@ public class EnemyStats : MonoBehaviour
     public int Morale; //Morale (from 0 - 100), 
                         //depending on this value, ATK/CRIT are boosted from +1 to +5 and HIT/AVO is boosted by +2 to +10
     public int MoraleMAX; //Maximum Morale
-    public int ATK; //Attack power (= STR - Player's DEF)
-    public int HIT; //Hit  (= (DEX*3 + LCK) / 2)
-    public int CRIT; //Critical  (= (DEX / 2) - 5)
-    public int AVO; //Avoid  (= (SPD*3 + LCK) / 2)
+    public int ATK; //Attack power (= STR - Player's DEF) +- difficultyAdjustment
+    public int HIT; //Hit Rate (= (((DEX*3 + LCK) / 2) - Player's AVO) +- difficultyAdjustment
+    public int CRIT; //Critical Rate (= ((DEX / 2) - 5) - Player's LCK) +- difficultyAdjustment
+    public int AVO; //Avoid  (= (SPD*3 + LCK) / 2) +- difficultyAdjustment
 
-    public int HitRate; // = HIT - Player's AVO
-    public int CritRate; // = CRIT - Player's LCK
+    public int difficultyAdjustment; // can be positive or negative. Will adjust enemy ATK, HIT, CRIT, and AVO parameters depending on difficulty setting.
+    public int difficultySetting; //5 difficulties from -2 (Very Easy) to +2 (Very Hard). diffcultyAdjustment = difficultySetting * 3
     
     public string Name;
 
@@ -72,11 +72,56 @@ public class EnemyStats : MonoBehaviour
         }
     }
 
+    //Attack the player, possibly with a critical hit
+    //Note: Critical hits triple the total damage
+    //Note: diffcultyAdjustment should be in the range of -6 to +6
     public int Attack(CharacterStats target){
 
-        ATK = (STR + (Morale / 5)) - target.DEF;
-        target.adjustHP(-ATK);
+        difficultyAdjustment = difficultySetting * 3;
+
+        HIT = ((((DEX * 3 + LCK) / 2) + (2 * (Morale / 5))) - target.AVO) + difficultyAdjustment;
+        CRIT = ((((DEX / 2) - 5) + (Morale / 5)) - target.LCK) + difficultyAdjustment;
+
+        if(determineCRIT(CRIT)){
+
+            ATK = (((STR + (Morale / 5)) - target.DEF) + difficultyAdjustment) * 3; //CRITICAL HIT!
+            target.adjustHP(-ATK);
+
+        }
+        else if(determineHIT(HIT)){
+            ATK = (((STR + (Morale / 5)) - target.DEF) + difficultyAdjustment); //HIT!
+            target.adjustHP(-ATK);
+
+        }
+        else{
+
+            ATK = 0; //Miss...
+            
+        }
+
 
         return ATK;
+    }
+
+    public bool determineHIT(int HIT){
+
+       
+        if(HIT >= Random.Range(0, 100)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public bool determineCRIT(int CRIT){
+
+        
+        if(CRIT >= Random.Range(0,100)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
