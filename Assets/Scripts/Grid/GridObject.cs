f5a9e8c61c37ffebddd294b1975961147c111e21
Zachariah Heath <heathz1@unlv.nevada.edu>
2022-12-09 00:38:46 -0800

    Functional cannons

--- Assets/Scripts/Grid/GridObject.cs
+++ Assets/Scripts/Grid/GridObject.cs
@@ -9,8 +9,15 @@ public class GridObject : MonoBehaviour
     public Vector2Int gridPosition;
     public bool isPlayerOwned;
     public bool overlapCharacter;
+    public InteractionType interactionType;
+    public string displayNamePrimary, displayNameSecondary;
 
     private bool isOnGrid = false;
+
+    public enum InteractionType
+    {
+        NONE, SINGLE, DOUBLE
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,5 +27,19 @@ public class GridObject : MonoBehaviour
             gridReference.GetComponent<GridBehavior>().AttachObjectToGrid(this.gameObject, gridPosition);
             isOnGrid = true;
         }
+    }
+
+    // Trigger this object's primary behavior when interacted with by a character, return created object to track with camera
+    public GameObject InteractPrimary(GameObject user)
+    {
+        Debug.Log("Empty primary");
+        return null;
+    }
+
+    // Trigger this object's secondary behavior when interacted with by a character, return created object to track with camera
+    public GameObject InteractSecondary(GameObject user)
+    {
+        Debug.Log("Empty secondary");
+        return null;
     }
 }
